fc02bd6348103333484eb61a26eefe2eed04cb9f
janehe <siyaoh4@uci.edu>
2024-04-18 08:14:18 -0700

    Migrate events to spans

--- core/src/main/java/com/datastax/oss/driver/internal/core/tracker/OtelRequestTracker.java
+++ core/src/main/java/com/datastax/oss/driver/internal/core/tracker/OtelRequestTracker.java
@@ -30,7 +30,9 @@ import com.datastax.oss.driver.api.core.metadata.Node;
 import com.datastax.oss.driver.api.core.session.Request;
 import com.datastax.oss.driver.api.core.tracker.RequestTracker;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.StatusCode;
 import io.opentelemetry.api.trace.Tracer;
 import io.opentelemetry.context.Context;
 import java.time.Instant;
@@ -39,7 +41,7 @@ import java.util.Objects;
 
 public class OtelRequestTracker implements RequestTracker {
 
-  private final Map<String, Span> logPrefixToSpanMap =
+  private final Map<String, TracingInfo> logPrefixToSpanMap =
       new java.util.concurrent.ConcurrentHashMap<>();
 
   private final Tracer tracer;
@@ -53,19 +55,28 @@ public class OtelRequestTracker implements RequestTracker {
   @Override
   public void onRequestHandlerCreated(
       @NonNull DriverContext context, @NonNull String requestLogPrefix) {
-    Span span = tracer.spanBuilder("Driver Internal Tracing").startSpan();
-    span.addEvent("Request handler created");
-    span.setAttribute("Session name", context.getSessionName());
-    span.setAttribute("CqlRequestHandler hashcode", requestLogPrefix);
-    logPrefixToSpanMap.put(requestLogPrefix, span);
+    Span parentSpan = tracer.spanBuilder("Cassandra Java Driver").startSpan();
+    parentSpan.addEvent("Request handler created");
+    parentSpan.setAttribute("Session name", context.getSessionName());
+    parentSpan.setAttribute("CqlRequestHandler hashcode", requestLogPrefix);
+    Span createdSpan =
+        tracer
+            .spanBuilder("Driver Processing Request")
+            .setParent(Context.current().with(parentSpan))
+            .startSpan();
+    TracingInfo tracingInfo = new TracingInfo();
+    tracingInfo.setParentSpan(parentSpan);
+    tracingInfo.setCreatedSpan(createdSpan);
+    logPrefixToSpanMap.put(requestLogPrefix, tracingInfo);
   }
 
   @Override
   public void onRequestSent(
       @NonNull Statement<?> statement, @NonNull Node node, @NonNull String requestLogPrefix) {
-    Span span = logPrefixToSpanMap.get(requestLogPrefix);
-    span.addEvent("Request sent");
-    span.setAttribute("Statement", statementToString(statement));
+    Span parentSpan = logPrefixToSpanMap.get(requestLogPrefix).getParentSpan();
+    parentSpan.setAttribute("Statement", statementToString(statement));
+    Span createdSpan = logPrefixToSpanMap.get(requestLogPrefix).getCreatedSpan();
+    createdSpan.end();
   }
 
   @Override
@@ -83,9 +94,50 @@ public class OtelRequestTracker implements RequestTracker {
       @NonNull String requestLogPrefix) {
     RequestTracker.super.onNodeError(
         request, error, latencyNanos, executionProfile, node, requestLogPrefix);
-    Span span = logPrefixToSpanMap.get(requestLogPrefix);
+    TracingInfo tracingInfo = logPrefixToSpanMap.get(requestLogPrefix);
+    if (tracingInfo == null) {
+      return;
+    }
+    Span span = tracingInfo.getParentSpan();
     span.recordException(error);
     span.end();
+    span.setStatus(StatusCode.ERROR);
+    logPrefixToSpanMap.remove(requestLogPrefix);
+  }
+
+  @Override
+  public void onSuccess(
+      @NonNull Request request,
+      long latencyNanos,
+      @NonNull DriverExecutionProfile executionProfile,
+      @NonNull Node node,
+      @NonNull String requestLogPrefix) {
+    TracingInfo tracingInfo = logPrefixToSpanMap.get(requestLogPrefix);
+    if (tracingInfo == null) {
+      return;
+    }
+    Span span = tracingInfo.getParentSpan();
+    span.end();
+    span.setStatus(StatusCode.OK);
+    logPrefixToSpanMap.remove(requestLogPrefix);
+  }
+
+  @Override
+  public void onError(
+      @NonNull Request request,
+      @NonNull Throwable error,
+      long latencyNanos,
+      @NonNull DriverExecutionProfile executionProfile,
+      @Nullable Node node,
+      @NonNull String requestLogPrefix) {
+    TracingInfo tracingInfo = logPrefixToSpanMap.get(requestLogPrefix);
+    if (tracingInfo == null) {
+      return;
+    }
+    Span span = tracingInfo.getParentSpan();
+    span.recordException(error);
+    span.end();
+    span.setStatus(StatusCode.ERROR);
     logPrefixToSpanMap.remove(requestLogPrefix);
   }
 
@@ -99,12 +151,13 @@ public class OtelRequestTracker implements RequestTracker {
       @NonNull AsyncResultSet resultSet) {
     RequestTracker.super.onNodeSuccess(
         request, latencyNanos, executionProfile, node, requestLogPrefix);
-    Span span = logPrefixToSpanMap.get(requestLogPrefix);
-    span.addEvent("Request success");
+    TracingInfo tracingInfo = logPrefixToSpanMap.get(requestLogPrefix);
+    if (tracingInfo == null) {
+      return;
+    }
+    Span span = tracingInfo.getParentSpan();
     // add cassandra query trace
-    // TODO: this must not be called on a driver thread
     if (resultSet.getExecutionInfo().getTracingId() != null) {
-
       new Thread(
               () -> {
                 QueryTrace queryTrace = resultSet.getExecutionInfo().getQueryTrace();
@@ -112,7 +165,10 @@ public class OtelRequestTracker implements RequestTracker {
                 span.end();
               })
           .start();
+    } else {
+      span.end();
     }
+    span.setStatus(StatusCode.OK);
     logPrefixToSpanMap.remove(requestLogPrefix);
   }
 
@@ -152,5 +208,26 @@ public class OtelRequestTracker implements RequestTracker {
 
     span.end(
         Instant.ofEpochMilli(queryTrace.getStartedAt() + queryTrace.getDurationMicros() / 1000));
+  }
+
+  private static class TracingInfo {
+    private Span parentSpan;
+    private Span createdSpan; // the span from handler created to request sent
+
+    private void setParentSpan(Span parentSpan) {
+      this.parentSpan = parentSpan;
+    }
+
+    private Span getParentSpan() {
+      return parentSpan;
+    }
+
+    private void setCreatedSpan(Span createdSpan) {
+      this.createdSpan = createdSpan;
+    }
+
+    private Span getCreatedSpan() {
+      return createdSpan;
+    }
   }
 }
