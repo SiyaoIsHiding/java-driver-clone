apache/cassandra-java-driver

Add ExecutionInfo to RequestTracker methods

core/src/main/java/com/datastax/oss/driver/internal/core/cql/CqlRequestHandler.java
@@ -338,13 +338,15 @@ public class CqlRequestHandler implements Throttled {
               nodeLatencyNanos,
               callback.executionProfile,
               callback.node,
-              logPrefix);
+              logPrefix,
+              executionInfo);
           requestTracker.onSuccess(
               callback.statement,
               totalLatencyNanos,
               callback.executionProfile,
               callback.node,
-              logPrefix);
+              logPrefix,
+              executionInfo);
         }
         if (sessionMetricUpdater.isEnabled(
             DefaultSessionMetric.CQL_REQUESTS, callback.executionProfile.getName())) {
@@ -431,27 +433,29 @@ public class CqlRequestHandler implements Throttled {
   private void setFinalError(Statement<?> statement, Throwable error, Node node, int execution) {
     DriverExecutionProfile executionProfile =
         Conversions.resolveExecutionProfile(statement, context);
+    ExecutionInfo executionInfo = null;
     if (error instanceof DriverException) {
-      ((DriverException) error)
-          .setExecutionInfo(
-              new DefaultExecutionInfo(
-                  statement,
-                  node,
-                  startedSpeculativeExecutionsCount.get(),
-                  execution,
-                  errors,
-                  null,
-                  null,
-                  true,
-                  session,
-                  context,
-                  executionProfile));
+      executionInfo =
+          new DefaultExecutionInfo(
+              statement,
+              node,
+              startedSpeculativeExecutionsCount.get(),
+              execution,
+              errors,
+              null,
+              null,
+              true,
+              session,
+              context,
+              executionProfile);
+      ((DriverException) error).setExecutionInfo(executionInfo);
     }
     if (result.completeExceptionally(error)) {
       cancelScheduledTasks();
       if (!(requestTracker instanceof NoopRequestTracker)) {
         long latencyNanos = System.nanoTime() - startTimeNanos;
-        requestTracker.onError(statement, error, latencyNanos, executionProfile, node, logPrefix);
+        requestTracker.onError(
+            statement, error, latencyNanos, executionProfile, node, logPrefix, executionInfo);
       }
       if (error instanceof DriverTimeoutException) {
         throttler.signalTimeout(this);

core/src/main/java/com/datastax/oss/driver/internal/core/tracker/MultiplexingRequestTracker.java
@@ -16,6 +16,7 @@
 package com.datastax.oss.driver.internal.core.tracker;
 
 import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
+import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.metadata.Node;
 import com.datastax.oss.driver.api.core.session.Request;
 import com.datastax.oss.driver.api.core.session.Session;
@@ -80,9 +81,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @NonNull Node node,
-      @NonNull String logPrefix) {
+      @NonNull String logPrefix,
+      @NonNull ExecutionInfo executionInfo) {
     invokeTrackers(
-        tracker -> tracker.onSuccess(request, latencyNanos, executionProfile, node, logPrefix),
+        tracker ->
+            tracker.onSuccess(
+                request, latencyNanos, executionProfile, node, logPrefix, executionInfo),
         logPrefix,
         "onSuccess");
   }
@@ -94,9 +98,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @Nullable Node node,
-      @NonNull String logPrefix) {
+      @NonNull String logPrefix,
+      @NonNull ExecutionInfo executionInfo) {
     invokeTrackers(
-        tracker -> tracker.onError(request, error, latencyNanos, executionProfile, node, logPrefix),
+        tracker ->
+            tracker.onError(
+                request, error, latencyNanos, executionProfile, node, logPrefix, executionInfo),
         logPrefix,
         "onError");
   }
@@ -107,9 +114,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @NonNull Node node,
-      @NonNull String logPrefix) {
+      @NonNull String logPrefix,
+      @NonNull ExecutionInfo executionInfo) {
     invokeTrackers(
-        tracker -> tracker.onNodeSuccess(request, latencyNanos, executionProfile, node, logPrefix),
+        tracker ->
+            tracker.onNodeSuccess(
+                request, latencyNanos, executionProfile, node, logPrefix, executionInfo),
         logPrefix,
         "onNodeSuccess");
   }

core/src/test/java/com/datastax/oss/driver/internal/core/loadbalancing/DefaultLoadBalancingPolicyRequestTrackerTest.java
@@ -20,6 +20,7 @@ import static com.datastax.oss.driver.api.core.config.DriverExecutionProfile.DEF
 import static org.mockito.BDDMockito.given;
 
 import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
+import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.session.Request;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableMap;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableSet;
@@ -32,6 +33,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
 
   @Mock Request request;
   @Mock DriverExecutionProfile profile;
+  @Mock ExecutionInfo executionInfo;
   final String logPrefix = "lbp-test-log-prefix";
 
   private DefaultLoadBalancingPolicy policy;
@@ -63,7 +65,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     nextNanoTime = 123;
 
     // When
-    policy.onNodeSuccess(request, 0, profile, node1, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node1, logPrefix, executionInfo);
 
     // Then
     assertThat(policy.responseTimes)
@@ -81,7 +83,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     nextNanoTime = 456;
 
     // When
-    policy.onNodeSuccess(request, 0, profile, node1, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node1, logPrefix, executionInfo);
 
     // Then
     assertThat(policy.responseTimes)
@@ -105,8 +107,8 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     nextNanoTime = 789;
 
     // When
-    policy.onNodeSuccess(request, 0, profile, node1, logPrefix);
-    policy.onNodeSuccess(request, 0, profile, node2, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node1, logPrefix, executionInfo);
+    policy.onNodeSuccess(request, 0, profile, node2, logPrefix, executionInfo);
 
     // Then
     assertThat(policy.responseTimes)

SiyaoIsHiding/java-driver-clone

core/src/main/java/com/datastax/oss/driver/internal/core/cql/CqlRequestHandler.java
@@ -340,12 +340,14 @@ public class CqlRequestHandler implements Throttled {
               nodeLatencyNanos,
               callback.executionProfile,
               callback.node,
+              executionInfo,
               logPrefix);
           requestTracker.onSuccess(
               callback.statement,
               totalLatencyNanos,
               callback.executionProfile,
               callback.node,
+              executionInfo,
               logPrefix);
         }
         if (sessionMetricUpdater.isEnabled(
@@ -433,27 +435,28 @@ public class CqlRequestHandler implements Throttled {
   private void setFinalError(Statement<?> statement, Throwable error, Node node, int execution) {
     DriverExecutionProfile executionProfile =
         Conversions.resolveExecutionProfile(statement, context);
+    ExecutionInfo executionInfo =
+        new DefaultExecutionInfo(
+            statement,
+            node,
+            startedSpeculativeExecutionsCount.get(),
+            execution,
+            errors,
+            null,
+            null,
+            true,
+            session,
+            context,
+            executionProfile);
     if (error instanceof DriverException) {
-      ((DriverException) error)
-          .setExecutionInfo(
-              new DefaultExecutionInfo(
-                  statement,
-                  node,
-                  startedSpeculativeExecutionsCount.get(),
-                  execution,
-                  errors,
-                  null,
-                  null,
-                  true,
-                  session,
-                  context,
-                  executionProfile));
+      ((DriverException) error).setExecutionInfo(executionInfo);
     }
     if (result.completeExceptionally(error)) {
       cancelScheduledTasks();
       if (!(requestTracker instanceof NoopRequestTracker)) {
         long latencyNanos = System.nanoTime() - startTimeNanos;
-        requestTracker.onError(statement, error, latencyNanos, executionProfile, node, logPrefix);
+        requestTracker.onError(
+            statement, error, latencyNanos, executionProfile, node, executionInfo, logPrefix);
       }
       if (error instanceof DriverTimeoutException) {
         throttler.signalTimeout(this);
@@ -947,7 +950,21 @@ public class CqlRequestHandler implements Throttled {
         nodeResponseTimeNanos = System.nanoTime();
       }
       long latencyNanos = nodeResponseTimeNanos - this.nodeStartTimeNanos;
-      requestTracker.onNodeError(statement, error, latencyNanos, executionProfile, node, logPrefix);
+      ExecutionInfo executionInfo =
+          new DefaultExecutionInfo(
+              statement,
+              node,
+              startedSpeculativeExecutionsCount.get(),
+              execution,
+              errors,
+              null,
+              null,
+              true,
+              session,
+              context,
+              executionProfile);
+      requestTracker.onNodeError(
+          statement, error, latencyNanos, executionProfile, node, executionInfo, logPrefix);
     }
 
     @Override

core/src/main/java/com/datastax/oss/driver/internal/core/tracker/MultiplexingRequestTracker.java
@@ -18,6 +18,7 @@
 package com.datastax.oss.driver.internal.core.tracker;
 
 import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
+import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.metadata.Node;
 import com.datastax.oss.driver.api.core.session.Request;
 import com.datastax.oss.driver.api.core.session.Session;
@@ -82,9 +83,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @NonNull Node node,
+      @NonNull ExecutionInfo executionInfo,
       @NonNull String logPrefix) {
     invokeTrackers(
-        tracker -> tracker.onSuccess(request, latencyNanos, executionProfile, node, logPrefix),
+        tracker ->
+            tracker.onSuccess(
+                request, latencyNanos, executionProfile, node, executionInfo, logPrefix),
         logPrefix,
         "onSuccess");
   }
@@ -96,9 +100,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @Nullable Node node,
+      @Nullable ExecutionInfo executionInfo,
       @NonNull String logPrefix) {
     invokeTrackers(
-        tracker -> tracker.onError(request, error, latencyNanos, executionProfile, node, logPrefix),
+        tracker ->
+            tracker.onError(
+                request, error, latencyNanos, executionProfile, node, executionInfo, logPrefix),
         logPrefix,
         "onError");
   }
@@ -109,9 +116,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @NonNull Node node,
+      @NonNull ExecutionInfo executionInfo,
       @NonNull String logPrefix) {
     invokeTrackers(
-        tracker -> tracker.onNodeSuccess(request, latencyNanos, executionProfile, node, logPrefix),
+        tracker ->
+            tracker.onNodeSuccess(
+                request, latencyNanos, executionProfile, node, executionInfo, logPrefix),
         logPrefix,
         "onNodeSuccess");
   }
@@ -123,10 +133,12 @@ public class MultiplexingRequestTracker implements RequestTracker {
       long latencyNanos,
       @NonNull DriverExecutionProfile executionProfile,
       @NonNull Node node,
+      ExecutionInfo executionInfo,
       @NonNull String logPrefix) {
     invokeTrackers(
         tracker ->
-            tracker.onNodeError(request, error, latencyNanos, executionProfile, node, logPrefix),
+            tracker.onNodeError(
+                request, error, latencyNanos, executionProfile, node, executionInfo, logPrefix),
         logPrefix,
         "onNodeError");
   }

core/src/test/java/com/datastax/oss/driver/internal/core/loadbalancing/DefaultLoadBalancingPolicyRequestTrackerTest.java
@@ -22,6 +22,7 @@ import static com.datastax.oss.driver.api.core.config.DriverExecutionProfile.DEF
 import static org.mockito.BDDMockito.given;
 
 import com.datastax.oss.driver.api.core.config.DriverExecutionProfile;
+import com.datastax.oss.driver.api.core.cql.ExecutionInfo;
 import com.datastax.oss.driver.api.core.session.Request;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableMap;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableSet;
@@ -34,6 +35,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
 
   @Mock Request request;
   @Mock DriverExecutionProfile profile;
+  @Mock ExecutionInfo executionInfo;
   final String logPrefix = "lbp-test-log-prefix";
 
   private DefaultLoadBalancingPolicy policy;
@@ -65,7 +67,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     nextNanoTime = 123;
 
     // When
-    policy.onNodeSuccess(request, 0, profile, node1, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node1, executionInfo, logPrefix);
 
     // Then
     assertThat(policy.responseTimes)
@@ -83,7 +85,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     nextNanoTime = 456;
 
     // When
-    policy.onNodeSuccess(request, 0, profile, node1, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node1, executionInfo, logPrefix);
 
     // Then
     assertThat(policy.responseTimes)
@@ -107,8 +109,8 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     nextNanoTime = 789;
 
     // When
-    policy.onNodeSuccess(request, 0, profile, node1, logPrefix);
-    policy.onNodeSuccess(request, 0, profile, node2, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node1, executionInfo, logPrefix);
+    policy.onNodeSuccess(request, 0, profile, node2, executionInfo, logPrefix);
 
     // Then
     assertThat(policy.responseTimes)
@@ -133,7 +135,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     Throwable iae = new IllegalArgumentException();
 
     // When
-    policy.onNodeError(request, iae, 0, profile, node1, logPrefix);
+    policy.onNodeError(request, iae, 0, profile, node1, executionInfo, logPrefix);
 
     // Then
     assertThat(policy.responseTimes)
@@ -152,7 +154,7 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     Throwable iae = new IllegalArgumentException();
 
     // When
-    policy.onNodeError(request, iae, 0, profile, node1, logPrefix);
+    policy.onNodeError(request, iae, 0, profile, node1, executionInfo, logPrefix);
 
     // Then
     assertThat(policy.responseTimes)
@@ -177,8 +179,8 @@ public class DefaultLoadBalancingPolicyRequestTrackerTest extends LoadBalancingP
     Throwable iae = new IllegalArgumentException();
 
     // When
-    policy.onNodeError(request, iae, 0, profile, node1, logPrefix);
-    policy.onNodeError(request, iae, 0, profile, node2, logPrefix);
+    policy.onNodeError(request, iae, 0, profile, node1, executionInfo, logPrefix);
+    policy.onNodeError(request, iae, 0, profile, node2, executionInfo, logPrefix);
 
     // Then
     assertThat(policy.responseTimes)