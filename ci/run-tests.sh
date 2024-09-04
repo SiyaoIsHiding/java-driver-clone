ca4069fd512df5e3ed9449ac404bf9982125611e
Siyao (Jane) He <jane.he@datastax.com>
2024-09-04 11:42:28 -0800

    Update run-tests.sh

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -7,4 +7,4 @@ printenv | sort
 mvn -B -V install -DskipTests -Dmaven.javadoc.skip=true
 jabba use ${TEST_JAVA_VERSION}
 printenv | sort
-mvn -B -V verify -T 1 -Ptest-jdk-${TEST_JAVA_MAJOR_VERSION} -DtestJavaHome=\$(jabba which ${TEST_JAVA_VERSION}) -Dccm.version=${SERVER_VERSION} -Dccm.dse=false -Dmaven.test.failure.ignore=true -Dmaven.javadoc.skip=true
+mvn -B -V verify -T 1 -Ptest-jdk-${TEST_JAVA_MAJOR_VERSION} -DtestJavaHome=$(jabba which ${TEST_JAVA_VERSION}) -Dccm.version=${SERVER_VERSION} -Dccm.dse=false -Dmaven.test.failure.ignore=true -Dmaven.javadoc.skip=true
